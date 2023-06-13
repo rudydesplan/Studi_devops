4030a2729b4ad6659aad7387ff0ae06e44cfa52e
rudy desplan <desrudy@hotmail.com>
2023-06-13 14:48:09 +0200

    Update main_linux.tf

--- Activity1/1/main_linux.tf
+++ Activity1/1/main_linux.tf
@@ -1,7 +1,9 @@
+# Local provider to start minikube
 provider "local" {
   version = "~> 2.4.0"
 }
 
+# Start minikube
 resource "local_exec" "install_minikube" {
   command = <<-EOF
     minikube start --vm-driver=none
