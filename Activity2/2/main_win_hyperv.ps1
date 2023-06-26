c8be6595b90a38a00ed625468292d0a7089b9b22
rudy desplan <desrudy@hotmail.com>
2023-06-26 20:24:09 +0200

    Create main_win_hyperv.ps1

---
+++ Activity2/2/main_win_hyperv.ps1
@@ -0,0 +1,62 @@
+# Define variables
+$IMAGE_NAME = "hello_world_app"
+$DEPLOYMENT_NAME = "hello_world_deployment"
+$SERVICE_NAME = "hello_world_service"
+
+# Check if the local Docker registry is already running
+if (-not (docker ps -q -f name=local-registry)) {
+    if (-not (docker ps -aq -f status=exited -f name=local-registry)) {
+        # Run the local Docker registry if it doesn't already exist
+        docker run -d -p 5000:5000 --name=local-registry registry:2
+    } else {
+        # Restart the local Docker registry container if it is stopped
+        docker start local-registry
+    }
+}
+
+# Go to the main directory
+Set-Location # Fill in the path where the Dockerfile is located
+
+# Build the Docker image
+docker build -t localhost:5000/$IMAGE_NAME:latest ./app
+
+# Check the Docker image
+docker run --name test-container -d -p 3000:3000 localhost:5000/$IMAGE_NAME:latest
+Start-Sleep -s 5 # Wait a few seconds for the application to start
+
+# Test the application
+$response = Invoke-WebRequest -UseBasicParsing -Uri http://localhost:3000
+if ($response.Content -match "Hello, World!") {
+    Write-Output "Test successful!"
+} else {
+    Write-Output "Test failed!" >&2
+    exit 1
+}
+
+# Stop and remove the test container
+docker stop test-container
+docker rm test-container
+
+# Push the Docker image
+docker push localhost:5000/$IMAGE_NAME:latest
+
+# Check if minikube is running
+$minikubeStatus = minikube status | Out-String
+if ($minikubeStatus -notmatch "host: Running") {
+    Write-Output "Minikube is not running. Please start minikube first."
+    exit 1
+}
+
+# Check if the minikube context exists
+$contexts = kubectl config get-contexts -o=name
+if ($contexts -notcontains "minikube") {
+    Write-Output "Minikube context does not exist. Please set up minikube."
+    exit 1
+}
+
+# Set the kubectl context to minikube
+kubectl config use-context minikube
+
+# Use kubectl to deploy the application
+kubectl run $DEPLOYMENT_NAME --image=localhost:5000/$IMAGE_NAME:latest --port=3000
+kubectl expose deployment $DEPLOYMENT_NAME --type=LoadBalancer --name=$SERVICE_NAME --port=3000
